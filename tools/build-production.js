247409e1c3b1c90bf94933704ae2820fa4b9588b
Alan Sakarjaew <snaiper1984@gmail.com>
2026-08-23 05:22:16 +0200

    fix: stamp production assets with commit hash

--- tools/build-production.js
+++ tools/build-production.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import { execSync } from 'node:child_process';
 import { fileURLToPath } from 'node:url';
 import { transformSync } from 'esbuild';
 
